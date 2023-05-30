6c09f2fe3c2c243c61363316e485cc6580adcbcf
Jesus Seijas <jseijas@gmail.com>
2023-05-30 12:41:28 +0200

    fix bug that created several primary memory

--- src/shared-memory.js
+++ src/shared-memory.js
@@ -1,7 +1,17 @@
 const cluster = require('cluster');
+const { ioc } = require('@toxo/ioc');
 const { PrimaryMemory } = require('./primary-memory');
 const { WorkerMemory } = require('./worker-memory');
 
+function getSharedMemory() {
+  let sharedMemory = ioc.get('sharedMemory');
+  if (!sharedMemory) {
+    sharedMemory = cluster.isPrimary ? new PrimaryMemory() : new WorkerMemory();
+    ioc.register('sharedMemory', sharedMemory);
+  }
+  return sharedMemory;
+}
+
 module.exports = {
-  sharedMemory: cluster.isPrimary ? new PrimaryMemory() : new WorkerMemory(),
+  getSharedMemory,
 };
